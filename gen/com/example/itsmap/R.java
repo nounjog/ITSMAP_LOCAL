85c77226628aeed0619998350aff99b6bba252b3
pierrelt <ple-texi@isep.fr>
2013-12-13 06:22:45 +0100

    ADD-TextView in FriendList

--- gen/com/example/itsmap/R.java
+++ gen/com/example/itsmap/R.java
@@ -216,15 +216,15 @@ containing a value of this type.
     public static final class id {
         public static final int ScrollView01=0x7f06000f;
         public static final int TableLayout01=0x7f060010;
-        public static final int action_settings=0x7f060016;
+        public static final int action_settings=0x7f060017;
         public static final int adduserbutton=0x7f06000c;
         public static final int cancelbutton=0x7f060008;
         public static final int createbutton=0x7f060009;
         public static final int hybrid=0x7f060004;
         public static final int list=0x7f06000e;
-        public static final int list2=0x7f060011;
-        public static final int map=0x7f060012;
-        public static final int name=0x7f060014;
+        public static final int list2=0x7f060012;
+        public static final int map=0x7f060013;
+        public static final int name=0x7f060015;
         public static final int newpassword=0x7f06000b;
         public static final int newusername=0x7f06000a;
         public static final int none=0x7f060000;
@@ -232,10 +232,11 @@ containing a value of this type.
         public static final int okbutton=0x7f060007;
         public static final int pager=0x7f06000d;
         public static final int password=0x7f060006;
-        public static final int refresh=0x7f060013;
+        public static final int refresh=0x7f060014;
         public static final int satellite=0x7f060002;
         public static final int terrain=0x7f060003;
-        public static final int timestamp=0x7f060015;
+        public static final int text=0x7f060011;
+        public static final int timestamp=0x7f060016;
         public static final int username=0x7f060005;
     }
     public static final class integer {
