004394490704de726bd5e5170f0f11d751bf6fbf
Thomas Bonte <thms.bonte@gmail.com>
2013-12-13 03:04:10 +0100

    ADD - SQL DataBase

--- src/com/example/itsmap/Friend/FriendListFragment.java
+++ src/com/example/itsmap/Friend/FriendListFragment.java
@@ -2,13 +2,30 @@ package com.example.itsmap.Friend;
 
 import java.util.ArrayList;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.itsmap.MySQLiteOpenHelper;
 import com.example.itsmap.R;
 import com.example.itsmap.ContentProdiver.CustomAdapter;
+import com.example.itsmap.DisplayService.GetLoc;
+import com.example.itsmap.Map.MapFragment;
 import com.example.itsmap.R.layout;
+import com.example.itsmap.UserManager.Login;
 
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
@@ -18,7 +35,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.Toast;
-
 
 public class FriendListFragment extends ListFragment {
 
@@ -30,131 +46,196 @@ public class FriendListFragment extends ListFragment {
 	CustomAdapter adapter;
 	Friend friend;
 	int currentPosition;
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-	    Log.d(TAG, "onCreateView");
-		View rootView = inflater.inflate(R.layout.fragment_friendlist, container, false);
-		
+		Log.d(TAG, "onCreateView");
+		View rootView = inflater.inflate(R.layout.fragment_friendlist,
+				container, false);
+
 		return rootView;
 	}
 
 	@Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-	    Log.d(TAG, "onActivityCreated");
-    	//String[] friends = { "Thomas", "Pierre", "Ivan" };
-    	//setListAdapter(new ArrayAdapter<String>(getActivity(), R.layout.simple_list, friends));
-	    
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		Log.d(TAG, "onActivityCreated");
+		new GetLoc().execute("http://pierrelt.fr/ITSMAP/getFriends.php?id="
+				+ Login.iduser);
+		// String[] friends = { "Thomas", "Pierre", "Ivan" };
+		// setListAdapter(new ArrayAdapter<String>(getActivity(),
+		// R.layout.simple_list, friends));
 
-	    datasource = new FriendsDataSource(getActivity().getApplicationContext());
-	    datasource.open();
+	}
 
-	    Log.d(TAG, "Getting all friends");
-	    friendList = datasource.getAllFriends();
-	    Log.d(TAG, "Gotten all friends");
-	    
-	    Friend friend1 = datasource.createFriend("Thomas");
-	    Friend friend2 = datasource.createFriend("Pierre");
-		   
-		    
-	    adapter = new CustomAdapter(getActivity(), friendList);
-	    adapter.add(friend1);
-	    adapter.add(friend2);
+	public class GetLoc extends AsyncTask<String, String, String> {
 
-	    Log.d(TAG, "First friend name in friendList: " + friendList.get(0).getName());
-	    
-	    Log.d(TAG, adapter.getItem(0).toString());
-	    
-	    setListAdapter(adapter);
-	    datasource.close();
-	    
-	    getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
-
-	        @Override
-	        public boolean onItemLongClick(AdapterView<?> parent, View view,
-	            int position, long id) {
-	            
-	            currentPosition = position;
-	            listDialog();
-	            return true;
-	        }
-	    });
 		
-    }
-	  
-	/**
-	 *  Shows dialog when ListView item is clicked
-	 *  for a long time.
-	 */
-	public void listDialog() {
-        String[] array = { "Delete" };  
-	    Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Actions");
-        builder.setItems(array, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int position) {
-                        deleteDialog();
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
-      
-	  }
-	  
-	/**
-	 * Dialog for deleting a friend
-	 */
-	public void deleteDialog() {
-	          Builder builder = new AlertDialog.Builder(getActivity());
-	          builder.setMessage("Do you want to delete this friend?");
-	          builder.setCancelable(true);
-	          builder.setNegativeButton("No", new CancelOnClickListener());
-	          builder.setPositiveButton("Yes", new OkOnClickListener());
-	          AlertDialog dialog = builder.create();
-	          dialog.show();
-	  }
-	    
-	/**
-	 * Cancel button listener that does nothing.
-	 */
-	private final class CancelOnClickListener implements
-	      DialogInterface.OnClickListener {
-	      public void onClick(DialogInterface dialog, int which) {
-	          
-	      }
-	  }
+		@Override
+		protected void onPostExecute(String result) {
+			Log.i("GetLoc", "OnCreate");
+			fill(result);
+			super.onPostExecute(result);
+			// Log.i("start", result);
+		}
 
-	/**
-	 * Button listener when deleting a friend in a dialog.
-	 *
-	 */
-	private final class OkOnClickListener implements
-	      DialogInterface.OnClickListener {
-	      public void onClick(DialogInterface dialog, int which) {
-  	      Toast.makeText(getActivity(), "Friend deleted",
-  	              Toast.LENGTH_LONG).show();
-            datasource.open();
-            friend = (Friend) getListAdapter().getItem(currentPosition);
-            datasource.deleteFriend(friend);
-            adapter.remove(friend);
-            datasource.close();
-            adapter.notifyDataSetChanged();
-	      }
-	  } 
-	  
-	  /**
-	   * Updates ListView.
-	  */
-	  public void updateListView() {
-	    datasource.open();
-	    friendList = datasource.getAllFriends();
-	    datasource.close();
-	    adapter.updateFriends(friendList);
-	    adapter.notifyDataSetChanged();
-	  }
-	 
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+		}
 
+		@Override
+		protected String doInBackground(String... params) {
+			Log.i("Background", "OnCreate");
+			try {
+				HttpClient httpclient = new DefaultHttpClient();
+				HttpGet method = new HttpGet(params[0]);
+				HttpResponse response = httpclient.execute(method);
+				HttpEntity entity = response.getEntity();
+				if (entity != null) {
+					return EntityUtils.toString(entity);
+				} else {
+					return "No string.";
+				}
+			} catch (Exception e) {
+				return "Network problem";
+			}
 
-	
+		}
+
+		public void fill(String o) {
+			Log.i("fill", "OnCreate");
+			Log.i("Result", o);
+			
+			try {
+				JSONArray jArray;
+				jArray = new JSONArray(o);
+				// Log.i("start", String.valueOf(jArray.length()));
+
+				for (int i = 0; i < jArray.length(); i++) {
+					JSONObject tmp = (JSONObject) jArray.get(i);
+					String val = tmp.get("name").toString();
+					int id = tmp.getInt("id");
+					Log.i("Nom", val);
+
+					addFriend(val, id);
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			// Log.i("start", "JSON FILLED");
+		}
+
+		public void addFriend(String name, int id) {
+
+			datasource = new FriendsDataSource(getActivity()
+					.getApplicationContext());
+			datasource.open();
+
+			Log.d(TAG, "Getting all friends");
+			friendList = datasource.getAllFriends();
+			Log.d(TAG, "Gotten all friends");
+
+			Friend friend1 = datasource.createFriend(name);
+
+			adapter = new CustomAdapter(getActivity(), friendList);
+			adapter.add(friend1);
+
+			Log.d(TAG, "First friend name in friendList: "
+					+ friendList.get(0).getName());
+
+			Log.d(TAG, adapter.getItem(0).toString());
+
+			setListAdapter(adapter);
+			datasource.close();
+
+			getListView().setOnItemLongClickListener(
+					new OnItemLongClickListener() {
+
+						@Override
+						public boolean onItemLongClick(AdapterView<?> parent,
+								View view, int position, long id) {
+
+							currentPosition = position;
+							listDialog();
+							return true;
+						}
+					});
+
+		}
+
+		/**
+		 * Shows dialog when ListView item is clicked for a long time.
+		 */
+		public void listDialog() {
+			String[] array = { "Delete" };
+			Builder builder = new AlertDialog.Builder(getActivity());
+			builder.setTitle("Actions");
+			builder.setItems(array, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int position) {
+					deleteDialog();
+				}
+			});
+			AlertDialog dialog = builder.create();
+			dialog.show();
+
+		}
+
+		/**
+		 * Dialog for deleting a friend
+		 */
+		public void deleteDialog() {
+			Builder builder = new AlertDialog.Builder(getActivity());
+			builder.setMessage("Do you want to delete this friend?");
+			builder.setCancelable(true);
+			builder.setNegativeButton("No", new CancelOnClickListener());
+			builder.setPositiveButton("Yes", new OkOnClickListener());
+			AlertDialog dialog = builder.create();
+			dialog.show();
+		}
+
+		/**
+		 * Cancel button listener that does nothing.
+		 */
+		private final class CancelOnClickListener implements
+				DialogInterface.OnClickListener {
+			public void onClick(DialogInterface dialog, int which) {
+
+			}
+		}
+
+		/**
+		 * Button listener when deleting a friend in a dialog.
+		 * 
+		 */
+		private final class OkOnClickListener implements
+				DialogInterface.OnClickListener {
+			public void onClick(DialogInterface dialog, int which) {
+				Toast.makeText(getActivity(), "Friend deleted",
+						Toast.LENGTH_LONG).show();
+				datasource.open();
+				friend = (Friend) getListAdapter().getItem(currentPosition);
+				datasource.deleteFriend(friend);
+				adapter.remove(friend);
+				datasource.close();
+				adapter.notifyDataSetChanged();
+			}
+		}
+
+		/**
+		 * Updates ListView.
+		 */
+		public void updateListView() {
+			datasource.open();
+			friendList = datasource.getAllFriends();
+			datasource.close();
+			adapter.updateFriends(friendList);
+			adapter.notifyDataSetChanged();
+		}
+
+	}
 }
--- src/com/example/itsmap/Friend/FriendsDataSource.java
+++ src/com/example/itsmap/Friend/FriendsDataSource.java
@@ -1,46 +1,60 @@
 package com.example.itsmap.Friend;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
-import com.example.itsmap.MySQLiteOpenHelper;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Looper;
 import android.util.Log;
+
+import com.example.itsmap.MySQLiteOpenHelper;
+import com.example.itsmap.UserManager.Login;
 
 public class FriendsDataSource {
 
-	  // Database fields
-	  private SQLiteDatabase db;
-	  private MySQLiteOpenHelper dbHelper;
-	  private String[] allColumns = { 
-	          MySQLiteOpenHelper.COLUMN_ID,
-    	      MySQLiteOpenHelper.COLUMN_NAME,
-    	      MySQLiteOpenHelper.COLUMN_TIMESTAMP };
+	// Database fields
+	private SQLiteDatabase db;
+	private MySQLiteOpenHelper dbHelper;
+	private String[] allColumns = { MySQLiteOpenHelper.COLUMN_ID,
+			MySQLiteOpenHelper.COLUMN_NAME, MySQLiteOpenHelper.COLUMN_TIMESTAMP };
 
-	  public FriendsDataSource(Context context) {
-	    dbHelper = new MySQLiteOpenHelper(context);
-	  }
+	public FriendsDataSource(Context context) {
+		dbHelper = new MySQLiteOpenHelper(context);
+		//db.delete(MySQLiteOpenHelper.TABLE_FRIEND, null, null);
+	}
 
 	/**
 	 * Opens database connection.
 	 * 
 	 * @throws SQLException
 	 */
-	  public void open() throws SQLException {
-	      db = dbHelper.getWritableDatabase();
-	  }
+	public void open() throws SQLException {
+		db = dbHelper.getWritableDatabase();
+		//db.delete(MySQLiteOpenHelper.TABLE_FRIEND, null, null);
 
+	}
 
 	/**
-	 *  Closes database connection.
+	 * Closes database connection.
 	 */
 	public void close() {
-	      dbHelper.close();
-	  }
+		dbHelper.close();
+	}
 
 	/**
 	 * Creates a new friend to database and returns it.
@@ -49,78 +63,138 @@ public class FriendsDataSource {
 	 * @return Created friend
 	 */
 	public Friend createFriend(String friendName) {
-	    Log.d("HabitsDataSource", "createFriend()");
-	    ContentValues values = new ContentValues();
-	    values.put(MySQLiteOpenHelper.COLUMN_NAME, friendName);
-	    values.put(MySQLiteOpenHelper.COLUMN_TIMESTAMP, "timestamp");
-	    long insertId = db.insert(MySQLiteOpenHelper.TABLE_FRIEND, null,
-	        values);
-	    Cursor cursor = db.query(MySQLiteOpenHelper.TABLE_FRIEND,
-	        allColumns, MySQLiteOpenHelper.COLUMN_ID + " = " + insertId, null,
-	        null, null, null);
-	    cursor.moveToFirst();
-	    Friend friend = cursorToFriend(cursor);
-	    cursor.close();
-	    return friend;
-	  }
-	  
+		Log.d("HabitsDataSource", "createFriend()");
+		ContentValues values = new ContentValues();
+		values.put(MySQLiteOpenHelper.COLUMN_NAME, friendName);
+		values.put(MySQLiteOpenHelper.COLUMN_TIMESTAMP, "Friend");
+		long insertId = db
+				.insert(MySQLiteOpenHelper.TABLE_FRIEND, null, values);
+		Cursor cursor = db.query(MySQLiteOpenHelper.TABLE_FRIEND, allColumns,
+				MySQLiteOpenHelper.COLUMN_ID + " = " + insertId, null, null,
+				null, null);
+		cursor.moveToFirst();
+		Friend friend = cursorToFriend(cursor);
+		cursor.close();
+		return friend;
+	}
+
 	/**
-	 * Updates an existing friend in the database and returns the modified friend.
+	 * Updates an existing friend in the database and returns the modified
+	 * friend.
 	 */
 	public Friend updateFriend(long rowId, String friendName, String timestamp) {
-	      Log.d("HabitsDataSource", "updateFriend()");
-	      ContentValues values = new ContentValues();
-	      values.put(MySQLiteOpenHelper.COLUMN_NAME, friendName);
-	      values.put(MySQLiteOpenHelper.COLUMN_TIMESTAMP, timestamp);
-	      db.update(MySQLiteOpenHelper.TABLE_FRIEND, values, MySQLiteOpenHelper.COLUMN_ID + "=" + rowId, null);
-	      Cursor cursor = db.query(MySQLiteOpenHelper.TABLE_FRIEND,
-	              allColumns, MySQLiteOpenHelper.COLUMN_ID + " = " + rowId, null,
-	              null, null, null);
-	      cursor.moveToFirst();
-	      Friend friend = cursorToFriend(cursor);
-	      cursor.close();
-	      return friend;
-	  }
+		Log.d("HabitsDataSource", "updateFriend()");
+		ContentValues values = new ContentValues();
+		values.put(MySQLiteOpenHelper.COLUMN_NAME, friendName);
+		values.put(MySQLiteOpenHelper.COLUMN_TIMESTAMP, timestamp);
+		db.update(MySQLiteOpenHelper.TABLE_FRIEND, values,
+				MySQLiteOpenHelper.COLUMN_ID + "=" + rowId, null);
+		Cursor cursor = db.query(MySQLiteOpenHelper.TABLE_FRIEND, allColumns,
+				MySQLiteOpenHelper.COLUMN_ID + " = " + rowId, null, null, null,
+				null);
+		cursor.moveToFirst();
+		Friend friend = cursorToFriend(cursor);
+		cursor.close();
+		return friend;
+	}
 
-	/** 
+	/**
 	 * Deletes friend from the database.
+	 * 
 	 * @param friend
 	 */
 	public void deleteFriend(Friend friend) {
-	    Log.d("FriendsDataSource", "deleteFriend()");
-	    long id = friend.getId();
-	    System.out.println("Friend deleted with id: " + id);
-	    db.delete(MySQLiteOpenHelper.TABLE_FRIEND, MySQLiteOpenHelper.COLUMN_ID
-	        + " = " + id, null);
-	  }
+		Log.d("FriendsDataSource", "deleteFriend()");
+		long id = friend.getId();
+		final String UPDATE_URL = "http://pierrelt.fr/ITSMAP/manageFriends.php";
+		final String Id = String.valueOf(id);
+		final String Iduser = Login.iduser;
+		System.out.println("Friend deleted with id: " + id);
+		db.delete(MySQLiteOpenHelper.TABLE_FRIEND, MySQLiteOpenHelper.COLUMN_ID
+				+ " = " + id, null);
+
+		Thread t = new Thread() {
+
+			public void run() {
+
+				Looper.prepare();
+				// On se connecte au serveur afin de communiquer avec le PHP
+				DefaultHttpClient client = new DefaultHttpClient();
+				HttpConnectionParams.setConnectionTimeout(client.getParams(),
+						15000);
+
+				HttpResponse response;
+				HttpEntity entity;
+
+				try {
+					// On établit un lien avec le script PHP
+					HttpPost post = new HttpPost(UPDATE_URL);
+
+					List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+
+					nvps.add(new BasicNameValuePair("id", Iduser));
+					nvps.add(new BasicNameValuePair("idf", Id));
+					nvps.add(new BasicNameValuePair("action", "deleteF"));
+					Log.i(Iduser, Id);
+					post.setHeader("Content-Type",
+							"application/x-www-form-urlencoded");
+					// On passe les paramètres login et password qui vont être
+					// récupérés
+					// par le script PHP en post
+					post.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+					// On récupère le résultat du script
+					response = client.execute(post);
+
+					entity = response.getEntity();
+
+					InputStream is = entity.getContent();
+					// On appelle une fonction définie plus bas pour traduire la
+					// réponse
+					is.close();
+
+					if (entity != null)
+						entity.consumeContent();
+
+				} catch (Exception e) {
+				}
+
+				Looper.loop();
+
+			}
+
+		};
+
+		t.start();
+	}
 
 	/**
 	 * Gets all the friends from the database.
+	 * 
 	 * @return List of friends
 	 */
 	public ArrayList<Friend> getAllFriends() {
-	    Log.d("FriendsDataSource", "getAllFriends()");
-	    ArrayList<Friend> friends = new ArrayList<Friend>();
+		Log.d("FriendsDataSource", "getAllFriends()");
+		ArrayList<Friend> friends = new ArrayList<Friend>();
 
-	    Cursor cursor = db.query(MySQLiteOpenHelper.TABLE_FRIEND,
-	        allColumns, null, null, null, null, null);
+		Cursor cursor = db.query(MySQLiteOpenHelper.TABLE_FRIEND, allColumns,
+				null, null, null, null, null);
 
-	    cursor.moveToFirst();
-	    while (!cursor.isAfterLast()) {
-	      Friend friend = cursorToFriend(cursor);
-	      friends.add(friend);
-	      cursor.moveToNext();
-	    }
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			Friend friend = cursorToFriend(cursor);
+			friends.add(friend);
+			cursor.moveToNext();
+		}
 
-	    cursor.close();
-	    return friends;
-	  }
+		cursor.close();
+		return friends;
+	}
 
-	  private Friend cursorToFriend(Cursor cursor) {
-	    Friend friend = new Friend();
-	    friend.setId(cursor.getLong(0));
-	    friend.setName(cursor.getString(1));
-	    friend.setTimestamp(cursor.getString(2));
-	    return friend;
-	  }
-	} 
+	private Friend cursorToFriend(Cursor cursor) {
+		Friend friend = new Friend();
+		friend.setId(cursor.getLong(0));
+		friend.setName(cursor.getString(1));
+		friend.setTimestamp(cursor.getString(2));
+		return friend;
+	}
+}
